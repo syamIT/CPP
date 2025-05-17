902dcc1c59dc26d9f7343734962cb337f2ab18a3
syamIT <syamdonkada255@gmail.com>
2025-05-17 19:35:27 +0530

    Patterns Created...

---
+++ first.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // std::cout<<"Hello World..."<<std::endl;
+    // std::cout<<"Hello bhai...."<<std::endl;
+    // int count;
+    // std::cin>>count;
+    // for (int  i = 0; i < count; i++)
+    // {
+    //     std::cout<<"*******"<<" ";
+    //     std::cout<<"\n";
+    // }
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     for (int j = 0; j < 5; j++)
+    //     {
+
+    //         cout << 10 << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    int row, col;
+    
+    // for(row = 0;row<4;row++){
+    //     for(col = 0;col<5;col++){
+    //         cout<<"10 ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for(row = 1;row<6;row++){
+    //     for(col = 1;col<6;col++){
+    //         cout<<row<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+
+    // for(row = 0;row <5;row++){
+    //     for(col = 1;col<=5;col++){
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+
+
+
+
+    // for (row = 0; row < 5; row++)
+    // {
+    //     for (col = 5; col > 0; col--)
+    //     {
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+
+
+
+    // for(row = 1;row < 6;row++){
+    //     for(col = 1;col <= 5;col++){
+    //         cout<<col*col<<" ";
+    //     }
+    //     cout<<"\n";
+    //   }
+
+
+
+
+    char ch = 'a' + 5;
+    cout<<ch<<endl;
+
+
+
+    
+    return 0;
+}
